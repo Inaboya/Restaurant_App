3c33890708dfb97e6607c1039de5752ff66fe0f6
Mac <mac@Macs-MacBook-Air-3.local>
2022-03-29 22:08:01 +0100

    implementing the spacer component

--- src/features/restuarants/components/restuarants.info.card.jsx
+++ src/features/restuarants/components/restuarants.info.card.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text, Image, View } from "react-native";
 import styled from "styled-components/native";
-import { View, Text, StyleSheet } from "react-native";
+import { SvgXml } from "react-native-svg";
 import { Card } from "react-native-paper";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestuarantsCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -11,6 +15,7 @@ const RestuarantsCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[2]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+
 const Title = styled.Text`
   font-family: ${(props) => props.theme.fonts.nanospace};
   font-size: ${(props) => props.theme.fontSizes.body};
@@ -20,31 +25,86 @@ const Title = styled.Text`
 const Address = styled.Text`
   font-family: ${(props) => props.theme.fonts.nanospace};
   font-size: ${(props) => props.theme.fontSizes.caption};
- `;
+`;
 
-const Info = styled.View` 
+const FlexDiv = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Open = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
 `;
 
 export const RestaurantsInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     ],
     address = "Some Address",
     rating = 4.5,
     isOpenNow = true,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
+
+  const newArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <RestuarantsCard elevation={5}>
-      <RestuarantsCardCover key={name} source={{ uri: photos[0] }} />
-      <Info>
-        <Title>{name}</Title>
-        <Address>{ address }</Address>
-      </Info>
-    </RestuarantsCard>
+    <>
+      <RestuarantsCard elevation={5}>
+        <RestuarantsCardCover key={name} source={{ uri: photos[0] }} />
+        <Info>
+          <Title>{name}</Title>
+
+          <FlexDiv>
+            <Rating>
+              {newArray.map((item, index) => (
+                <SvgXml
+                  key={index}
+                  xml={star}
+                  style={{ width: 20, height: 20 }}
+                />
+              ))}
+            </Rating>
+
+            <Open>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+
+              {isOpenNow && (
+                <Spacer position="left" size="large">
+                  <SvgXml xml={open} style={{ width: 20, height: 20 }} />
+                </Spacer>
+              )}
+
+              <Spacer position="left" size="large">
+                <Image
+                  style={{ height: 15, width: 15 }}
+                  source={{ uri: icon }}
+                />
+              </Spacer>
+            </Open>
+          </FlexDiv>
+
+          <Address>{address}</Address>
+        </Info>
+      </RestuarantsCard>
+    </>
   );
 };
